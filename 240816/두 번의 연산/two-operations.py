c4338a10139169ac94da74596929a4ae9419b5ea
leebee33 <leewyg@naver.com>
2024-08-16 17:30:42 +0900

    Title: 두 번의 연산, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240816/두 번의 연산/two-operations.py
+++ 240816/두 번의 연산/two-operations.py
@@ -1,8 +1,7 @@
-a = int(input())
-if a%2 !=0:
-    if (a+3)%3==0:
-        print (f'{(a+3)//3}')
-
-if a%2==0:
-    if a%3==0:
-        print(f'{a//3}')
+b= input()
+a =int(b)
+if  a % 2 == 1 :
+    a +=3
+    if a % 3 == 0 :
+        a//=3
+        print(a)
